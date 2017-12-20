547c0c946dd492f1db93f4b75a87229cbb37e07e
Kelsey Yuen <heavenslashinghell@gmail.com>
2017-12-20 00:45:47 -0500

    Refactored RequestDAO
    Now inherits from abstract DAO which has generic methods

---
+++ src/main/java/com/staed/dao/DAO.java
@@ -0,0 +1,98 @@
+package com.staed.dao;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.CallableStatement;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public abstract class DAO {
+    /**
+     * Creates a SQL connection and creates a PreparedStatement of the query
+     * @param sql - The SQL query desired
+     * @return A PreparedStatement of the query
+     */
+    PreparedStatement prepareStatement(String sql) throws SQLException {
+        return prepare(sql, PreparedStatement.class);
+    }
+
+    /**
+     * Creates a SQL connection and creates a CallableStatement of the query
+     * @param sql - The SQL query desired
+     * @return A CallableStatement of the query
+     */
+    CallableStatement prepareCallable(String sql) throws SQLException {
+        return prepare(sql, CallableStatement.class);
+    }
+
+    /**
+     * Create the SQL connection and return either a PreparedStatement
+     * or CallableStatement of the SQL query
+     * @param String - The SQL query desired
+     * @param Class&lt;T&gt; - A PreparedStatement or CallableStatement class
+     * @return Either a PreparedStatement or CallableStatement
+     */
+    <T extends Statement> T prepare(String sql, Class<T> type) throws SQLException {
+        Connection conn = getConnection();
+
+        if (type.isInstance(PreparedStatement.class))
+            return (T) conn.prepareStatement(sql);
+        else if (type.isInstance(CallableStatement.class))
+            return (T) conn.prepareCall(sql);
+        else
+            return null;
+    }
+
+    /**
+     * Given a SQL query, it will either process it as a PreparedStatement
+     * or a CallableStatement. If the query is a PreparedStatement, this
+     * will return a list<Obj> of the result set whose size is indeterminate.
+     * If the query is a CallableStatement, it will be executed and return an
+     * empty list<Obj>.
+     * @param T - A type extending from java.sql.Statement, handled only if
+     * it is a PreparedStatement or CallableStatement.
+     * @return A List&lt;U&gt; and is either null or of indeterminate size.
+     */
+    <T extends Statement, U> List<U> resultIterator(T stmt) throws SQLException {
+        List<U> list = new ArrayList<>();
+
+        if (stmt.getClass().isInstance(CallableStatement.class)) {
+            ((CallableStatement) stmt).execute();
+        } else if (stmt.getClass().isInstance(PreparedStatement.class)) {
+            ResultSet rs = ((PreparedStatement) stmt).executeQuery();
+            while (rs.next()) {
+                list.add(extractRow(rs));
+            }
+            rs.close();
+        }
+        stmt.close();
+
+        return list;
+    }
+
+    abstract<T> T extractRow(ResultSet rs) throws SQLException;
+
+    protected static Connection getConnection() {
+        try {
+            Properties prop = new Properties();
+            InputStream in = new FileInputStream("connection.properties");
+            prop.load(in);
+
+            String url = prop.getProperty("url");
+            String user = prop.getProperty("user");
+            String password = prop.getProperty("password");
+
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- src/main/java/com/staed/dao/RequestDAO.java
+++ src/main/java/com/staed/dao/RequestDAO.java
@@ -2,106 +2,97 @@ package com.staed.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.CallableStatement;
 import java.sql.Date;
 
 import com.staed.beans.*;
 import com.staed.enums.EventType;
 import com.staed.enums.GradingFormat;
-//import com.staed.factory.*;
-import com.staed.*;
 
-public class RequestDAO {
-    //private static RequestFactory rf = new RequestFactory();
+public class RequestDAO extends DAO {
+    private String joinedTables = "FROM REQUEST R INNER JOIN EVENTTYPE EVT ON EVT.EVENTTYPEID = R.EVENTTYPEID INNER JOIN GRADINGFORMAT G ON G.GRADINGFORMATID = R.GRADINGFORMATID INNER JOIN STATUS S ON S.STATUSID = R.STATUSID INNER JOIN EMPLOYEE E ON E.EMPLOYEEID = R.EMPLOYEEID INNER JOIN DEPARTMENT D ON D.DEPARTMENTID = E.DEPARTMENTID INNER JOIN TITLE T ON T.TITLEID = E.TITLEID";
 
-    public Request getRequest(int rId) {
-        PreparedStatement ps = null;
-        Request request = null;
-        ResultSet rs = null;
+    public Request getRequest(int id) throws SQLException {
+        String sql = "SELECT * " + joinedTables + " WHERE REQUESTID = ?";
+        PreparedStatement ps = prepareStatement(sql);
+        ps.setInt(1, id);
+        List<Request> rI = resultIterator(ps);
+        return rI.isEmpty() ? null : rI.get(0);
+    }
 
-        try (Connection conn = ConnectionUtil.getConnection()) {
-            StringBuilder sqlBuild = new StringBuilder();
-            sqlBuild.append("SELECT * FROM REQUEST R ");
-            sqlBuild.append("INNER JOIN EVENTTYPE E ");
-            sqlBuild.append("ON R.EVENTTYPEID = E.EVENTTYPEID ");
+    public List<Request> getAllRequest() throws SQLException {
+        String sql = "SELECT * " + joinedTables;
+        return resultIterator(prepareStatement(sql));
+    }    
 
-            sqlBuild.append("INNER JOIN GRADINGFORMAT G ");
-            sqlBuild.append("ON R.GRADINGFORMATID = G.GRADINGFORMATID ");
+    public int _DeleteRequest(int id) throws SQLException {
+        String sql = "DELETE FROM REQUEST WHERE REQUESTID = ?";
+        CallableStatement cs = prepareCallable(sql);
+        cs.setInt(1, id);
+        resultIterator(cs);
+        return cs.getInt(1); // TODO: Might be invalid here
+    }
 
-            sqlBuild.append("INNER JOIN STATUS S ");
-            sqlBuild.append("ON R.STATUSID = S.STATUSID ");
-
-            sqlBuild.append("WHERE REQUESTID = ?");
-
-            String sql = sqlBuild.toString();
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, rId);
-            rs = ps.executeQuery();
-            while(rs.next()) {
-                int employeeId = rs.getInt("EMPLOYEEID");
-                Date evtTime = rs.getDate("EVENTTIME");
-                String evtLoc = rs.getString("EVENTLOCATION");
-                String requestDesc = rs.getString("REQUESTDESCRIPTION");
-                float cost = rs.getFloat("REIMBURSEMENTCOST");
-                
-                GradingFormat format = GradingFormat.Grade;
-                if (rs.getString("GRADINGFORMATNAME").toLowerCase().equals("presentation")) {
-                    format = GradingFormat.Presentation;
-                }
-
-                EventType evtType;
-                switch (rs.getString("EVENTTYPENAME").toLowerCase()) {
-                    case "university course":
-                        evtType = EventType.UniversityCourse;
-                        break;
-                    case "seminar":
-                        evtType = EventType.Seminars;
-                        break;
-                    case "certification preparation course":
-                        evtType = EventType.CertificationPreparation;
-                        break;
-                    case "certification":
-                        evtType = EventType.Certification;
-                        break;
-                    case "technical training":
-                        evtType = EventType.TechnicalTraining;
-                        break;
-                    default:
-                        evtType = EventType.Other;
-                        break;
-                }
-                
-                String justification = rs.getString("WORKJUSTIFICATION");
-                String email = rs.getString("APPROVALEMAIL");
-                
-                Boolean okdSuper = false;
-                if (rs.getInt("DIRECTSUPERVISORAPPROVED") > 0)
-                    okdSuper = true;
-                Boolean okdHead = false;
-                if (rs.getInt("DEPARTMENTHEADAPPROVED") > 0)
-                    okdHead = true;
-                Boolean okdBenCo = false;
-                if (rs.getInt("BENEFITSCOORDINATORAPPROVED") > 0)
-                    okdBenCo = true;
-
-                int cutoff = rs.getInt("GRADECUTOFF");
-                String status = rs.getString("STATUSNAME");
-                
-                Boolean urgent = false;
-                if (rs.getInt("URGENT") > 0) {
-                    urgent = true;
-                }
-
-                request = new Request(rId, employeeId, evtTime, evtLoc,
-                    requestDesc, cost, format, evtType, justification, email,
-                    okdSuper, okdHead, okdBenCo, cutoff, status, urgent);
-            }
-            rs.close();
-            ps.close();
-        } catch (Exception ex) {
-            ex.getMessage();
+    Request extractRow(ResultSet rs) throws SQLException {
+        int requestId = rs.getInt("REQUESTID");
+        int employeeId = rs.getInt("EMPLOYEEID");
+        Date evtTime = rs.getDate("EVENTTIME");
+        String evtLoc = rs.getString("EVENTLOCATION");
+        String requestDesc = rs.getString("REQUESTDESCRIPTION");
+        float cost = rs.getFloat("REIMBURSEMENTCOST");
+        
+        GradingFormat format = GradingFormat.Grade;
+        if (rs.getString("GRADINGFORMATNAME").toLowerCase().equals("presentation")) {
+            format = GradingFormat.Presentation;
         }
 
-        return request;
+        EventType evtType;
+        switch (rs.getString("EVENTTYPENAME").toLowerCase()) {
+            case "university course":
+                evtType = EventType.UniversityCourse;
+                break;
+            case "seminar":
+                evtType = EventType.Seminars;
+                break;
+            case "certification preparation course":
+                evtType = EventType.CertificationPreparation;
+                break;
+            case "certification":
+                evtType = EventType.Certification;
+                break;
+            case "technical training":
+                evtType = EventType.TechnicalTraining;
+                break;
+            default:
+                evtType = EventType.Other;
+                break;
+        }
+        
+        String justification = rs.getString("WORKJUSTIFICATION");
+        String email = rs.getString("APPROVALEMAIL");
+        
+        Boolean okdSuper = false;
+        if (rs.getInt("DIRECTSUPERVISORAPPROVED") > 0)
+            okdSuper = true;
+        Boolean okdHead = false;
+        if (rs.getInt("DEPARTMENTHEADAPPROVED") > 0)
+            okdHead = true;
+        Boolean okdBenCo = false;
+        if (rs.getInt("BENEFITSCOORDINATORAPPROVED") > 0)
+            okdBenCo = true;
+
+        int cutoff = rs.getInt("GRADECUTOFF");
+        String status = rs.getString("STATUSNAME");
+        
+        Boolean urgent = false;
+        if (rs.getInt("URGENT") > 0) {
+            urgent = true;
+        }
+
+        return new Request(requestId, employeeId, evtTime, evtLoc,
+            requestDesc, cost, format, evtType, justification, email,
+            okdSuper, okdHead, okdBenCo, cutoff, status, urgent);
     }
 }
